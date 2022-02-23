9a19e2d820375a5ea2f232eac9ecd5ae149ca67a
JeffLi <lidfdsffd49@gmail.com>
2022-02-23 15:13:22 +0800

    update db

--- restaurant/src/main/java/pkpd/restaurant/Enums/ResultEnum.java
+++ restaurant/src/main/java/pkpd/restaurant/Enums/ResultEnum.java
@@ -8,26 +8,26 @@ package pkpd.restaurant.Enums;
  */
 
 public enum ResultEnum {
-    UNKNOWN_ERROR(-1,"未知错误!"),
-    SUCCESS(200,"成功!"),
-    USER_NO_FOUND(101,"账号未找到!"),
-    PWD_ERROR(101,"密码错误!"),
-    USER_IS_EXIST(101,"账号已注册！"),
-    PHONE_IS_EXIST(101,"电话号已存在!"),
-    ID_NUMBER_IS_EXIST(101,"身份证号已存在！"),
-    MC_NAME_IS_EXIST(101,"会员类型已存在！"),
-    DESK_CODE_IS_EXIST(101,"桌位编号已存在！"),
-    DESK_CODE_NO_EXIST(101,"桌位编号未找到！"),
-    DESK_CODE_NO_IDLE(101,"桌位不空闲！"),
-    CATEGORY_NAME_IS_EXIST(101,"菜品类型已存在!"),
-    GOODS_NAME_IS_EXIST(101,"菜品名称已存在！"),
-    FIELD_VALIDATION_FAILS(202,"验证失败！"),
-    INSERT_DB_FAIL(101,"数据添加失败！"),
-    DEL_DB_FAIL(101,"删除失败！"),
-    UPDATE_DB_FAIL(101,"修改失败！"),
-    IMAGE_UPLOAD_FAIL(101,"图片上传失败！"),
-    UNAUTHENTICATED_ERROR(100,"没有权限"),
-    ADD_ORDER_FAIL(101,"添加订单失败");
+    UNKNOWN_ERROR(-1,"Unknown Error!"),
+    SUCCESS(200,"Successful!"),
+    USER_NO_FOUND(101,"Cannot find the account!"),
+    PWD_ERROR(101,"Error password!"),
+    USER_IS_EXIST(101,"The account has existed!"),
+    PHONE_IS_EXIST(101,"The phone number has existed!"),
+    ID_NUMBER_IS_EXIST(101,"The ID has existed!"),
+    MC_NAME_IS_EXIST(101,"The member type has existed!"),
+    DESK_CODE_IS_EXIST(101,"The desk ID has existed!"),
+    DESK_CODE_NO_EXIST(101,"Cannot find the desk ID!"),
+    DESK_CODE_NO_IDLE(101,"The desk is not idle!"),
+    CATEGORY_NAME_IS_EXIST(101,"The goods type has existed!"),
+    GOODS_NAME_IS_EXIST(101,"The goods name has existed!"),
+    FIELD_VALIDATION_FAILS(202,"Field validation fails!"),
+    INSERT_DB_FAIL(101,"Insert database failed!"),
+    DEL_DB_FAIL(101,"Delete database failed!"),
+    UPDATE_DB_FAIL(101,"Update database failed!"),
+    IMAGE_UPLOAD_FAIL(101,"Image upload failed!"),
+    UNAUTHENTICATED_ERROR(100,"Unauthorized!"),
+    ADD_ORDER_FAIL(101,"    Add order failed!");
     private Integer code;
     private String msg;
 
